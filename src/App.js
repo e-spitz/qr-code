7138b483e70b8356297545206d93524e41165466
Erica <ericaspitz522@gmail.com>
2024-04-09 17:52:16 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center bg-lightgray h-screen">
+      <section className="flex flex-col justify-center items-center text-center w-[20rem] m-10 sm:w-[17rem] bg-white drop-shadow-lg rounded-md">
+        <div className="p-4">
+          <img src="./image-qr-code.png" alt="QR Code" className="rounded-md"/>
+        </div>
+        <div className="font-outfit mb-6 px-2">
+          <p className="text-[22px] font-bold text-darkblue md:drop-shadow-md mb-2">Improve your front-end skills by building projects</p>
+          <p className="text-[15px] font-normal text-gray">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      lightgray: 'hsl(212, 45%, 89%)',
+      grayblue: 'hsl(220, 15%, 55%)',
+      darkblue: 'hsl(218, 44%, 22%)',
+      gray: '#7D889E'
+  },
+  fontFamily: {
+    outfit: ["Outfit", "sans-serif"],
+  },
+  },
+  plugins: [],
+}
+
